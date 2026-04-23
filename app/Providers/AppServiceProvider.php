f29039600812934ea86a02bc36a1f9e7290ff9ef
sxaksaa <akbarsalahudinpurnomo@gmail.com>
2026-04-23 19:11:32 +0700

    hapus

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL; // 🔥 INI YANG KURANG
-use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,7 +17,5 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
-        // 🔥 AUTO MIGRATE
-        Artisan::call('migrate', ['--force' => true]);
     }
 }
